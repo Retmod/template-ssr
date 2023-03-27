816cb55c160e44333bca57457ff8c1b2eea02810
RedCrafter07 <gh@r07.dev>
2023-03-27 17:07:10 +0200

    Add some paths

--- .retmod/paths.ts
+++ .retmod/paths.ts
@@ -1,7 +1,14 @@
 import path from 'path';
 
+const cwd = process.cwd();
+
 const paths = {
-	rootFolder: path.join(__dirname, '..'),
+	rootFolder: path.join(cwd),
+	htmlTemplate: path.join(cwd, 'src', 'util', 'template.html'),
+	clientFolder: path.join(cwd, 'src', 'client'),
+	clientBuildFolder: path.join(cwd, '.retmod', 'dist', 'client'),
+	serverFolder: path.join(cwd, 'src', 'server'),
+	serverBuildFolder: path.join(cwd, '.retmod', 'dist', 'server'),
 };
 
 export default paths;
