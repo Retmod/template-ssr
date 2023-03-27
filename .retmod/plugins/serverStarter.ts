4985a15dee3bff1976fde835dedf834452840db5
RedCrafter07 <gh@r07.dev>
2023-03-27 17:07:25 +0200

    Optimize syntax and add log support

--- .retmod/plugins/serverStarter.ts
+++ .retmod/plugins/serverStarter.ts
@@ -1,35 +1,54 @@
 import { spawn } from 'child_process';
 import * as webpack from 'webpack';
 
-export default function serverStarter(compiler: webpack.Compiler) {
-	// if the compiler is not in watch mode, return
-	if (!compiler.options.watch) return;
+class ServerStarterPlugin {
+	apply(compiler: webpack.Compiler) {
+		// if the compiler is not in watch mode, return
+		if (!compiler.options.watch) return;
 
-	// check if compiler has finished compiling
-	let isCompiled = false;
-	compiler.hooks.done.tap('done', () => {
-		isCompiled = true;
-	});
+		// check if compiler has finished compiling
+		let isCompiled = false;
+		compiler.hooks.done.tap('done', () => {
+			isCompiled = true;
+		});
 
-	// create a new child process
-	let childProcess = spawn('node', ['.retmod/dist/server/main.js'], {
-		stdio: 'inherit',
-	});
+		// create a new child process
+		let childProcess = spawn('node', ['.retmod/dist/server/main.js'], {
+			stdio: 'inherit',
+		});
 
-	// watch for changes in the compiler
-	compiler.hooks.watchRun.tap('watchRun', () => {
-		// if the compiler has finished compiling, restart the child process
-		if (isCompiled) {
+		// log out the child process's stdout
+		childProcess.stdout?.on('data', (data) => {
+			console.log(data.toString());
+		});
+
+		// watch for changes in the compiler
+		compiler.hooks.watchRun.tap('watchRun', () => {
+			// if the compiler has finished compiling, restart the child process
+			if (isCompiled) {
+				// kill the child process
+				childProcess.kill();
+
+				// create a new child process
+				childProcess = spawn('node', ['.retmod/dist/server/main.js'], {
+					stdio: 'inherit',
+				});
+
+				childProcess.stdout?.on('data', (data) => {
+					console.log(data.toString());
+				});
+
+				// set isCompiled to false
+				isCompiled = false;
+			}
+		});
+
+		// watch for the compiler to close
+		compiler.hooks.watchClose.tap('watchClose', () => {
 			// kill the child process
 			childProcess.kill();
-
-			// create a new child process
-			childProcess = spawn('node', ['.retmod/dist/server/main.js'], {
-				stdio: 'inherit',
-			});
-
-			// set isCompiled to false
-			isCompiled = false;
-		}
-	});
+		});
+	}
 }
+
+export default ServerStarterPlugin;
